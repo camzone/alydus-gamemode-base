f3ab1b4548f91ad7184e291dac69e5a055b410a2
Alydus <officialalydus@gmail.com>
2017-02-06 18:48:18 +0000

    Folders within server/client/shared, auto folder

--- alydusbase/gamemode/cl_init.lua
+++ alydusbase/gamemode/cl_init.lua
@@ -1,15 +1,98 @@
 include("shared.lua")
 
-local gamemodeFolderName = "alydusbase"
+// Variables
+local doConsoleMessages = true // Show include information on init?
+local gamemodeFolderName = "alydusbase" // The gamemode folder name
 
-// Include all clientside files.
-for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/cl_*.lua", "LUA")) do
-	include("client/" .. v)
-	print("Including client/" .. v .. " in cl_init.lua...")
+local clientFiles, clientDirectories = file.Find(gamemodeFolderName .. "/gamemode/client/*", "LUA")
+local sharedFiles, sharedDirectories = file.Find(gamemodeFolderName .. "/gamemode/shared/*", "LUA")
+local autoFiles, autoDirectories = file.Find(gamemodeFolderName .. "/gamemode/auto/*", "LUA")
+
+// Functions
+local function includeFolder(side, folder)
+	if side == "client" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/" .. folder .. "/cl_*.lua", "LUA")) do
+			include("client/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'client/" .. folder.. "/" .. v .. "' in cl_init.lua")
+			end
+		end
+	elseif side == "shared" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/shared/" .. folder .. "/sh_*.lua", "LUA")) do
+			include("shared/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'shared/" .. folder.. "/" .. v .. "' in cl_init.lua")
+			end
+		end
+	elseif side == "auto" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/" .. folder .. "/sh_*.lua", "LUA")) do
+			include("auto/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'auto/" .. folder.. "/" .. v .. "' in cl_init.lua")
+			end
+		end
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/" .. folder .. "/cl_*.lua", "LUA")) do
+			include("auto/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'auto/" .. folder.. "/" .. v .. "' in cl_init.lua")
+			end
+		end
+	end
 end
 
-// Include all shared files.
+// Clientside
+for k,v in pairs(clientDirectories) do
+	includeFolder("client", v)
+	if doConsoleMessages then
+		print("Checking for files in auto directory 'client/" .. v .. "' in cl_init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/cl_*.lua", "LUA")) do
+	include("client/" .. v)
+	if doConsoleMessages then
+		print("Including 'client/" .. v .. "' in cl_init.lua")
+	end
+end
+
+// Shared
+for k,v in pairs(sharedDirectories) do
+	includeFolder("shared", v)
+	if doConsoleMessages then
+		print("Checking for files in shared directory 'shared/" .. v .. "' in cl_init.lua")
+	end
+end
+
 for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/shared/sh_*.lua", "LUA")) do
 	include("shared/" .. v)
-	print("Including shared/" .. v .. " in cl_init.lua...")
-end 
+	if doConsoleMessages then
+		print("Including 'shared/" .. v .. "' in cl_init.lua")
+	end
+end
+
+// Auto Folder
+for k,v in pairs(autoDirectories) do
+	includeFolder("auto", v)
+	if doConsoleMessages then
+		print("Checking for files in auto directory 'auto/" .. v .. "' in cl_init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/cl_*.lua", "LUA")) do
+	include("auto/" .. v)
+	if doConsoleMessages then
+		print("Including 'auto/" .. v .. "' in cl_init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/sh_*.lua", "LUA")) do
+	include("auto/" .. v)
+	if doConsoleMessages then
+		print("Including 'auto/" .. v .. "' in cl_init.lua")
+	end
+end
+
+// Finish clientside init
+if doConsoleMessages then
+	print("Finished loading cl_init.lua")
+end
--- alydusbase/gamemode/init.lua
+++ alydusbase/gamemode/init.lua
@@ -3,30 +3,147 @@ AddCSLuaFile("shared.lua")
 
 include("shared.lua")
 
-print("Loading init.lua...")
+// Variables
+local doConsoleMessages = true // Show include information on init?
+local gamemodeFolderName = "alydusbase" // The gamemode folder name
 
-local gamemodeFolderName = "alydusbase"
+local clientFiles, clientDirectories = file.Find(gamemodeFolderName .. "/gamemode/client/*", "LUA")
+local sharedFiles, sharedDirectories = file.Find(gamemodeFolderName .. "/gamemode/shared/*", "LUA")
+local serverFiles, serverDirectories = file.Find(gamemodeFolderName .. "/gamemode/server/*", "LUA")
+local autoFiles, autoDirectories = file.Find(gamemodeFolderName .. "/gamemode/auto/*", "LUA")
 
-// AddCSLuaFile all clientside files.
-for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/cl_*.lua", "LUA")) do
-	AddCSLuaFile("client/" .. v)
-	print("Making client/" .. v .. " avaliable for client download...")
+if doConsoleMessages then
+	print("Loading init.lua")
 end
 
-// AddCSLuaFile all shared files.
+// Functions
+local function includeFolder(side, folder)
+	if side == "server" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/server/" .. folder .. "/sv_*.lua", "LUA")) do
+			include("server/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'server/" .. folder.. "/" .. v .. "' in init.lua")
+			end
+		end
+	elseif side == "client" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/" .. folder .. "/cl_*.lua", "LUA")) do
+			AddCSLuaFile("client/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Making 'client/" .. folder.. "/" .. v .. "' avaliable for client in init.lua")
+			end
+		end
+	elseif side == "shared" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/shared/" .. folder .. "/sh_*.lua", "LUA")) do
+			AddCSLuaFile("shared/" .. folder .. "/" .. v)
+			include("shared/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Making 'shared/" .. folder.. "/" .. v .. "' avaliable for client in init.lua")
+				print("Including 'shared/" .. folder.. "/" .. v .. "' in init.lua")
+			end
+		end
+	elseif side == "auto" then
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/" .. folder .. "/sh_*.lua", "LUA")) do
+			AddCSLuaFile("auto/" .. folder .. "/" .. v)
+			include("auto/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Making 'auto/" .. folder.. "/" .. v .. "' avaliable for client in init.lua")
+				print("Including 'auto/" .. folder.. "/" .. v .. "' in init.lua")
+			end
+		end
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/" .. folder .. "/cl_*.lua", "LUA")) do
+			AddCSLuaFile("auto/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Making 'auto/" .. folder.. "/" .. v .. "' avaliable for client in init.lua")
+			end
+		end
+		for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/" .. folder .. "/sv_*.lua", "LUA")) do
+			include("auto/" .. folder .. "/" .. v)
+			if doConsoleMessages then
+				print("Including 'auto/" .. folder.. "/" .. v .. "' in init.lua")
+			end
+		end
+	end
+end
+
+// Clientside
+for k,v in pairs(clientDirectories) do
+	includeFolder("client", v)
+	if doConsoleMessages then
+		print("Checking for files in client directory 'client/" .. v .. "'")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/client/cl_*.lua", "LUA")) do
+	include("client/" .. v)
+	if doConsoleMessages then
+		print("Making 'client/" .. v .. "' avaliable for client in init.lua")
+	end
+end
+
+// Shared
+for k,v in pairs(sharedDirectories) do
+	includeFolder("shared", v)
+	if doConsoleMessages then
+		print("Checking for files in shared directory 'shared/" .. v .. "' in init.lua")
+	end
+end
+
 for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/shared/sh_*.lua", "LUA")) do
 	AddCSLuaFile("shared/" .. v)
-	print("Making shared/" .. v .. " avaliable for client download...")
+	include("shared/" .. v)
+	if doConsoleMessages then
+		print("Making 'shared/" .. v .."' avaliable for client in init.lua")
+		print("Including 'shared/" .. v .. "' in init.lua")
+	end
 end
 
-// Include all serverside files
+// Serverside
+for k,v in pairs(serverDirectories) do
+	includeFolder("server", v)
+	if doConsoleMessages then
+		print("Checking for files in server directory 'server/" .. v .."' in init.lua")
+	end
+end
+
 for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/server/sv_*.lua", "LUA")) do
 	include("server/" .. v)
-	print("Including server/" .. v .. " in init.lua...")
+	if doConsoleMessages then
+		print("Including 'server/" .. v .. "' in init.lua")
+	end
 end
 
-// Include all shared files.
-for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/shared/sh_*.lua", "LUA")) do
-	include("shared/" .. v)
-	print("Including shared/" .. v .. " in init.lua...")
+// Auto Folder
+for k,v in pairs(autoDirectories) do
+	includeFolder("auto", v)
+	if doConsoleMessages then
+		print("Checking for files in auto directory 'auto/" .. v .. "' in init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/sv_*.lua", "LUA")) do
+	include("auto/" .. v)
+	if doConsoleMessages then
+		print("Including 'auto/" .. v .. "' in init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/cl_*.lua", "LUA")) do
+	AddCSLuaFile("auto/" .. v)
+	if doConsoleMessages then
+		print("Making 'auto/" .. v .. "' avaliable for client in init.lua")
+	end
+end
+
+for k,v in pairs(file.Find(gamemodeFolderName .. "/gamemode/auto/sh_*.lua", "LUA")) do
+	include("auto/" .. v)
+	AddCSLuaFile("auto/" .. v)
+	if doConsoleMessages then
+		print("Including 'auto/" .. v .. "' in init.lua")
+		print("Making 'auto/" .. v .. "' avaliable for client in init.lua")
+	end
+end
+
+// Finish serverside init
+if doConsoleMessages then
+	print("Finished loading init.lua")
 end
